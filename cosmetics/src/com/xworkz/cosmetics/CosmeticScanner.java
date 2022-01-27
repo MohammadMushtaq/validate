ebb09be81725927b1e49b50d395e0f42ae5f79a3
unknown <mushtaqmech.rymec@gmail.com>
2022-01-27 12:28:58 +0530

    upadate

--- cosmetics/src/com/xworkz/cosmetics/CosmeticScanner.java
+++ cosmetics/src/com/xworkz/cosmetics/CosmeticScanner.java
@@ -15,15 +15,23 @@ public class CosmeticScanner {
 		
 		CosmeticEntity cosmeticEntity= new CosmeticEntity("mushtaq", "null", 8214550382l, "bangalore", 583101,"male", 1, "trimer", 2, 2450.5, "hair remove",LocalDate.now(),"mushtaq",LocalDateTime.now(),"naveen",LocalDateTime.now(),"password");
 		
-		CosmeticEntity coEntity = new CosmeticEntity("mushtaq", "mushtaq.xworkz@gmail.com", 821745054l, "bangalore", 583101,"male", 1, "trimer", 2, 2450.5, "hair remove",LocalDate.now(),"mushtaq",LocalDateTime.now(),"password");
+		CosmeticEntity coEntity = new CosmeticEntity("mushtaq", "mushtaq.xworkz@gmail.com", 8217450387l, "bangalore", 583101,"male", 1, "trimer", 2, 2450.5, "hair remove",LocalDate.now(),"mushtaq",LocalDateTime.now(),"mushtaq@1");
+		CosmeticEntity coEntity1 = new CosmeticEntity("ahamad", "ahamad.xworkz@gmail.com", 921745054l, "bellary", 583102,"male", 2, "comb", 3, 245.5, "hairstyle",LocalDate.now(),"mushtaq",LocalDateTime.now(),"password@123");
+		CosmeticEntity coEntity2 = new CosmeticEntity("priya", "priya.xworkz@gmail.com", 9217148547l, "mysore", 583105,"female", 4, "nailpolish", 3, 245.5, "madicure",LocalDate.now(),"mushtaq",LocalDateTime.now(),"priya@654");
+		CosmeticEntity coEntity3 = new CosmeticEntity("kajol", "kaljol.xworkz@gmail.com", 8874531668l, "hospet", 583715,"female", 5, "hair dryer", 3, 245.5, "electronic hairstyle",LocalDate.now(),"mushtaq",LocalDateTime.now(),"kajol.sm");
+		CosmeticEntity coEntity4 = new CosmeticEntity("anusha", "anusharymec@gmail.com", 948057866l, "hampi", 583615,"female", 7, "make up set", 5, 1456, "Allproduct",LocalDate.now(),"mushtaq",LocalDateTime.now(),"anushap&@123");
+
 		
-//		CosmeticDAO cosmeticDAO= new CosmeticDAOImpl();
-//		cosmeticDAO.save(cosmeticEntity1);
+		CosmeticDAO cosmeticDAO= new CosmeticDAOImpl();
+	//	cosmeticDAO.save(coEntity4);
 		
-		CosmeticService cosmeticService = new CosmeticServiceImpl();
-		cosmeticService.validateAndSave(coEntity);
+
 		
+		//		CosmeticService cosmeticService = new CosmeticServiceImpl();
+//		cosmeticService.validateAndSave(coEntity);
+//		
 	
+		
 	}
 
 }
--- cosmetics/src/com/xworkz/cosmetics/service/CosmeticServiceImpl.java
+++ cosmetics/src/com/xworkz/cosmetics/service/CosmeticServiceImpl.java
@@ -9,98 +9,129 @@ import com.xworkz.cosmetics.entity.CosmeticEntity;
 
 public class CosmeticServiceImpl implements CosmeticService {
 
-	private CosmeticDAO dao = new CosmeticDAOImpl();
-
+	//private CosmeticDAO dao = new CosmeticDAOImpl();
+	private CosmeticDAO dao;
+	CosmeticServiceImpl() {
+		this.dao= dao;
+	}
 	@Override
 	public boolean validateAndSave(CosmeticEntity entity) {
 		try {
 			boolean valid = true;
-			
-		if (entity.getName() != null && entity.getName().length() > 3 && entity.getName().length() < 25
-					) {
 
-				if (entity.getEmail() != null&& entity.getEmail().endsWith(".com")) {
+			if (entity.getName() != null && entity.getName().length() > 3 && entity.getName().length() < 25) {
+				valid = true;
+			} else {
+				
+				System.err.println("invalid name ");
+				System.err.println("chararcter >3&<25");
+				return false;
+			}
 
-					if (entity.getPhonenumber() != 0 && entity.getPhonenumber() > 0 && entity.getPhonenumber() <= 10000000000l) {
+			if (entity.getEmail() != null && entity.getEmail().length()>4&&entity.getEmail().contains("@")&&entity.getEmail().contains(".")) {
+				valid = true;
+			} else {
+				System.err.println("invalid email");
+				System.err.println("chararcter >3 & < 25 contain '.' '@'");
+				return false;
+			}
+			if (entity.getPhonenumber() != 0 && entity.getPhonenumber()>1000000000l
+					&& entity.getPhonenumber() < 10000000000l) {	
+				valid = true;
+			} else {							 						
+				System.err.println("phonenum not valid");
+				System.err.println("number >9&<10");
+				return false;
+			}
+			if (entity.getAddress() != null && entity.getAddress().length() > 5 && entity.getAddress().length() < 50) {
+				valid = true;
+			} else {
+				System.err.println("invalid address");
+				System.err.println("chararcter >5&<50");
+				return false;
+			}
+			if (entity.getPincode() != 0 && entity.getPincode() > 3 && entity.getPincode() < 1000000) {
+				valid = true;
+			} else {																		 
+				System.err.println("invalid pincode");
+				
+				return false;
+			}
+			if (entity.getGender() != null && entity.getGender().contains("male")||entity.getGender().contains("female")
+					||entity.getGender().contains("other")) {
+				valid = true;
+			} else {
+				System.err.println("invalid gender");
+				System.err.println("all lower case");
+				return false;
+			}
+			if (entity.getOId() != 0 && entity.getOId() > 0 && entity.getOId() < 1000) {
+				valid = true;
+			} else {
+				System.err.println("invalid oredrid");
+				return false;
+			}
 
-						if (entity.getAddress() != null && entity.getAddress().length() > 5
-								&& entity.getAddress().length() < 50) {
+			if (entity.getPname() != null && entity.getPname().length() > 2 && entity.getPname().length() < 15) {
+				valid = true;
+			} else {
+				System.err.println("invalid product name");
+				System.err.println("character >2");
+				return false;
+			}
+			if (entity.getPquantity() != 0 && entity.getPquantity() > 0 && entity.getPquantity() < 1000) {
+				valid = true;
+			} else {
+				System.err.println("invalid quantity");
+				return false;
+			}
+			if (entity.getPrice() != 0 && entity.getPrice() > 0) {
+				valid = true;
+			} else {
+				System.err.println("price not valid");
+				return false;
+			}
 
-							if (entity.getPincode() != 0 && entity.getPincode() > 3 && entity.getPincode() < 80215615) {
+			if (entity.getPtype() != null) {
+				valid = true;
+			} else {
+				System.err.println("type not valid");
+				return false;
+			}
+			if (entity.getLocalDate() != null) {
+				valid = true;
+			} else {
+				System.err.println("invalid date");
+				return false;
+			}
 
-								if (entity.getGender() != null && entity.getGender().length() > 3
-										&& entity.getGender().length() < 6) {
+			if (entity.getCreatedBy() != null && entity.getCreatedBy().length()>3 ) {
+				valid = true;
+			} else {
+				System.err.println("not valid name");
+				System.err.println("chararcter >3");
 
-									if (entity.getOId() != 0 && entity.getOId() > 0 && entity.getOId() < 5) {
+				return false;
+			}
+			if (entity.getCreatedAt() != null) {
+				valid = true;
+			} else {
+				System.err.println("not valid dateandtime");
+				return false;
+			}
+			if (entity.getPassword() != null && entity.getPassword().length() > 4 
+					&& entity.getPassword().length() > 14 && entity.getPassword().contains("&")||
+					entity.getPassword().contains("@")||entity.getPassword().contains(".")) {
+				valid = true;
+			} else {
+				System.err.println("invalid password");
+				System.err.println("must contain './@/&' '>4&<14'");
 
-										if (entity.getPname() != null && entity.getPname().length()>3) {
-
-											if (entity.getPquantity() != 0 && entity.getPquantity() > 0
-													&& entity.getPquantity() < 1000) {
-
-												if (entity.getPrice() != 0 && entity.getPrice() > 0) {
-
-													if (entity.getPtype() != null) {
-
-														if (entity.getLocalDate() != null) {
-
-															if (entity.getCreatedBy() != null) {
-
-																if (entity.getCreatedAt() != null) {
-																	
-																	if(entity.getPassword()!=null && entity.getPassword().length()>4) {
-																		if (valid)dao.save(entity);
-																	}
-																		else {
-																		System.err.println("invalid password");
-																	}
-																}
-																	else {
-																		System.err.println("not valid dateandtime");
-																	}
-																} else {
-																	System.err.println("not valid name");
-																}
-															} else {
-																System.err.println("invalid date");
-															}
-														} else {
-															System.err.println("type not valid");
-														}
-													}
-
-													else {
-														System.err.println("price not valid");
-													}
-												} else {
-													System.err.println("invalid quantity");
-												}
-											} else {
-												System.err.println("invalid product name");
-											}
-										} else {
-											System.err.println("invalid oredrid");
-										}
-									} else {
-										System.err.println("invalid gender");
-									}
-								} else {
-									System.err.println("invalid pincode");
-								}
-							} else {
-								System.err.println("invalid address");
-							}
-						} else {
-							System.err.println("phonenum not valid");
-						}
-
-					} else {
-						System.err.println("invalid email");
-					}
-				} else {
-					System.err.println("invalid name");
-				}
-
+				return false;
+			}
+			if (valid)
+				dao.save(entity);
+			return true;
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
